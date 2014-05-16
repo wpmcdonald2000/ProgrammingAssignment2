1424ce1944747a71a7ca71dc14ca0550e92c5f23
wpmcdonald2000 <wpmcdonald2000@gmail.com>
2014-05-15 19:27:30 -0700

    Coursera R Programming Assignment 2
    Matrix Inversion operations
    create matrix,
    calculate inverted matrix
    store inverted matrix results

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix function creates a matrix from input ...
+# creates a list of functions to be called by cacheSolve function ...
+# and sets inverse as NULL or caches the results of cacheSolve matrix inversion
+#  
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+        # initialize m_inverse as NULL ... the inverse matrix
+        m_inverse <- NULL
+        
+        # initialize the matrix
+        setm <- function(y) {
+                m <<- y
+                m_inverse <<- NULL
+        }
+        
+        #functions to retrieve created matrix, set values for the inverse
+        getm <- function() m
+        setinv <- function(inv) m_inverse <<- inv
+        getinv <- function() m_inverse
+        list(set = setm, getm = getm, setinv = setinv, getinv = getinv)
 }
 
+# cacheSolve function takes matrix as input, 
+# checks for existing inverse or calculates inverse
+# returns inverse
 
-## Write a short comment describing this function
+cacheSolve <- function(m, ...) {
+        # retrieve the value of getinv in makeCacheMatirx function
+        m_inverse <- m$getinv()
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #  Check if inverse matrix has been calculated
+        if(!is.null(m_inverse)) {
+                # if yes,  return that result
+                message("been there, done that, here's what I've got")
+                return(m_inverse)
+        }
+        
+        # if not, get matrix from makeCacheMatrix function ...    
+        data <- m$getm()
+        
+        # and calculate the inverse
+        m_inverse <- solve(data, ...)
+        
+        # set the inverse value in makeCacheMarix function
+        m$setinv(m_inverse)
+        
+        m_inverse
 }
